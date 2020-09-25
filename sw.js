bee16b9986305722822e2977e7380618a0878c17
AnishBastakoti <anishbastakoti7@gmail.com>
2020-09-25 22:50:07 +0930

    added improvised sw

--- sw.js
+++ sw.js
@@ -54,20 +54,40 @@ const urlsToCache = [
   '/src/img/spider.png',
 ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(cacheName)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+self.addEventListener("install", (e) => {
+  console.log("Service Worker: Installed");
+
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then((cache) => {
+        console.log("Service Worker: Caching Files");
+        cache.addAll(cacheAssets);
       })
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request) ||
-      fetch(event.request)
+// Call Activate Event
+self.addEventListener("activate", (e) => {
+  console.log("Service Worker: Activated");
+  // Remove unwanted caches
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            console.log("Service Worker: Clearing Old Cache");
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
+});
+
+// Call Fetch Event
+self.addEventListener("fetch", (e) => {
+  console.log("Service Worker: Fetching");
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
