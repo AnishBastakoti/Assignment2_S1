002fca7d4e740fe7f1e99d81c7285efd0b7295a5
AnishBastakoti <anishbastakoti7@gmail.com>
2020-09-25 22:32:13 +0930

    modified

--- src/js/main1.js
+++ src/js/main1.js
@@ -1,6 +1,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('../../sw.js/').then(function(registration) {
+      scope: '../build/views/mainpage.html'
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
